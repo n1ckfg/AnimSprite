85b849fe394253d36f84b586188a9ca9632bcaba
Nick Fox-Gieg <nick@fox-gieg.com>
2017-10-12 21:06:43 -0400

    py update

--- processingpy/AnimSpriteExample/Bacterium.py
+++ processingpy/AnimSpriteExample/Bacterium.py
@@ -1,41 +1,41 @@
 class Bacterium extends AnimSprite{
  
  Bacterium():
-     super("bacterium",12,50,50,10,10);
-     init();
+     super("bacterium",12,50,50,10,10)
+     init()
  }
 
  Bacterium(PImage[] _name){
-     super(_name,12);
-     init();
+     super(_name,12)
+     init()
  }
  
  def init():
-     super.init();
-     p = new PVector(sW/2,sH/2,0);
+     super.init()
+     p = new PVector(sW/2,sH/2,0)
  }
  
  def update():
      if(mousePressed){
-         p = tween3D(p, new PVector(mouseX,mouseY,0), new PVector(random(10,100),random(10,100),10));
-         t = new PVector(random(sW),random(sH),random(sD)-(sD/2));
+         p = tween3D(p, new PVector(mouseX,mouseY,0), new PVector(random(10,100),random(10,100),10))
+         t = new PVector(random(sW),random(sH),random(sD)-(sD/2))
      }else{
-         p = tween3D(p, t, new PVector(random(10,100),random(10,100),10));
+         p = tween3D(p, t, new PVector(random(10,100),random(10,100),10))
      }
      if(hitDetect(p.x,p.y,s.x,s.x,t.x,t.y,s.x,s.y)){
-             t = new PVector(random(sW),random(sH),random(sD)-(sD/2));
+             t = new PVector(random(sW),random(sH),random(sD)-(sD/2))
          }
 
-     super.update();
+     super.update()
  }
  
  def draw():
-     super.draw();
+     super.draw()
  }
  
  def run():
-     update();
-     draw();
+     update()
+     draw()
  }
     
 }
